9e58243e62dc5bfd1855805dd4c809ad07885a0d
MefjuS22 <smydamateusz9@gmail.com>
2023-01-10 23:03:22 +0100

    Minor styling changes with Modal

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -22,30 +22,30 @@ export const ModalComponent = () => {
       header={header}
       onHide={onHide}
       visible={isOpen}
-      className="w-10 md:w-8 lg:w-6 xl:w-3"
+      className="w-10 md:w-8 lg:w-7 xl:w-5 dialog"
       dismissableMask
     >
       <div className="flex flex-column gap-3 align-items-center">
         <div
-          className="p-2 bg-gray-900 w-full sm:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
+          className="flex-grow-1 p-2 bg-gray-900 w-full xl:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
           style={{ borderColor: `${modalData?.color}` }}
         >
-          Nake: {modalData?.name}
+          Name: {modalData?.name}
         </div>
         <div
-          className="p-2 bg-gray-900 w-full sm:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
+          className="flex-grow-1 p-2 bg-gray-900 w-full xl:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
           style={{ borderColor: `${modalData?.color}` }}
         >
           Year: {modalData?.year}
         </div>
         <div
-          className="p-2 bg-gray-900 w-full sm:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
+          className="flex-grow-1 p-2 bg-gray-900 w-full xl:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
           style={{ borderColor: `${modalData?.color}` }}
         >
           Color: {modalData?.color}
         </div>
         <div
-          className="p-2 bg-gray-900 w-full sm:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
+          className="flex-grow-1 p-2 bg-gray-900 w-full xl:w-6 text-lg flex justify-content-center align-items-center h-3rem border-round-lg shadow-1 border-top-3"
           style={{ borderColor: `${modalData?.color}` }}
         >
           Pantone value: {modalData?.pantone_value}
